0ab1112bae28366fa964d02f2873d789e9668116
Re-Ja-Rocha <135358234+Re-Ja-Rocha@users.noreply.github.com>
2023-11-20 10:59:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// CONFIGURANDO INPUT PARA TORNAR A PAGINA MODO NOTURNO
+const inputCheck = document.querySelector("#modo-noturno")
+const elemento = document.querySelector('body')
+
+inputCheck.addEventListener('click',() => {
+    const modo = inputCheck.checked ?'dark' : 'ligth'
+    elemento.setAttribute("data-bs-theme", modo)
+}   )
